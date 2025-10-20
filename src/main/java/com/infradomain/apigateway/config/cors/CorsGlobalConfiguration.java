ea6a6593f700d9da1e5ed36cc8f1f6142d3308f3
JuhMaran <julianemaran@gmail.com>
2025-10-20 19:07:58 -0300

    chore: atualiza as configurações do CORS

--- src/main/java/com/infradomain/apigateway/config/cors/CorsGlobalConfiguration.java
+++ src/main/java/com/infradomain/apigateway/config/cors/CorsGlobalConfiguration.java
@@ -1,5 +1,6 @@
-package com.infradomain.apigateway.config;
+package com.infradomain.apigateway.config.cors;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
@@ -13,10 +14,14 @@ import org.springframework.web.reactive.config.WebFluxConfigurer;
 @Configuration
 public class CorsGlobalConfiguration implements WebFluxConfigurer {
 
+  @Value("${gateway.cors.allowed-origins}")
+  private String allowedOrigins;
+
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-      .allowedOrigins("http://localhost:4200")
+      .allowedOrigins(allowedOrigins.split(",")) // suporta múltiplos origins separados por vírgula
+//      .allowedOrigins("http://localhost:4200")
       .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
       .allowedHeaders("*")
       .exposedHeaders("*")
